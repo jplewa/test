bfdee669566ec811f8dba820807ea08af5156194
Julia Plewa <jplewa@virtuslab.com>
2023-08-30 12:56:28 +0200

    fghjk

---
+++ out/test/test/bind_to_all_interfaces.go
@@ -0,0 +1,12 @@
+package directory
+import (
+    "log"
+    "net"
+)
+func main() {
+    l, err := net.Listen("tcp", "0.0.0.0:2000")
+    if err != nil {
+        log.Fatal(err)
+    }
+    defer l.Close()
+}
